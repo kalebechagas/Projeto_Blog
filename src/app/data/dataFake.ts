57b1bfe0b5e22c5b95ee0a85243824f01becf6eb
Kalebe <kalebewlk@gmail.com>
2023-05-20 11:56:34 -0300

    Blog finished

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "GAME MATA MOSQUITO COM JS",
+    "description": "Este é um jogo simples de mata-mosquitos criado com JavaScript. O objetivo do jogo é matar o maior número possível de mosquitos em 15 segundos.",
+    "photoCover":"href=../../../assets/mata_mosquito.png"
+  },
+  {
+    "id":"2",
+    "title": "Mario Jump (GAME OVER)",
+    "description": "Este é um jogo básico do Mario feito com HTML, CSS e JavaScript. O objetivo do jogo é praticar a aprimorar minhas habilidades.",
+    "photoCover":"href=../../../assets/mario_jump.png"
+  },
+  {
+    "id":"3",
+    "title": "Pokedex simples com JS",
+    "description": "Esta é uma Pokedex simples desenvolvida em HTML, CSS e Javascript que exibe informações básicas sobre os Pokémons. A Pokedex é uma ferramenta fundamental para qualquer treinador de Pokémon, pois fornece informações úteis sobre cada Pokémon, como tipo, habilidades, movimentos, evoluções e muito mais.",
+    "photoCover":"href=../../../assets/pokedex.png"
+  },
+  {
+    "id":"4",
+    "title": "Projeto Spotify clone",
+    "description": "Este é um clone a página inicial do Spotify, uma plataforma de streaming de música muito popular em todo o mundo. O objetivo deste projeto é demonstrar minhas habilidades de programação e desenvolvimento web, recriando algumas das funcionalidades básicas do Spotify.",
+    "photoCover":"href=../../../assets/spotify.png"
+  },
+]
